77cda79a3aa9b842ec52069762814613797852b3
Muhammad Ahmed Memon <ahmedmemon0766@gmail.com>
2024-02-18 21:19:51 +0500

    Add: Redis Updated Code, CmakeLists.txt and Build Script fix

--- BuildProject.bat
+++ BuildProject.bat
@@ -3,14 +3,16 @@
 cd hiredis
 mkdir build
 cd build
-cmake ..
+cmake -DBUILD_SHARED_LIBS=OFF --fresh ..
 cmake --build .
-mkdir ..\lib\
+cd ..\..
+mkdir lib
+cd hiredis\build
 copy Debug\* ..\..\lib
 cd ..\..
 cd redis-plus-plus
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH="%cd%\..\.." -DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF  ..
-cmake --build .
+cmake -DCMAKE_PREFIX_PATH="%cd%\..\.." -DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_CXX_STANDARD=17 -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF  -DCMAKE_BUILD_TYPE=Debug --fresh ..
+cmake --build . 
 copy Debug\* ..\..\lib
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
+#include <string>
+#include <chrono>
+#include <tuple>
 #include <iostream>
+#include "sw/redis++/redis++.h"
 
+using namespace sw::redis;
+#pragma comment(lib, "Ws2_32.lib")
 int main(){
-    std::cout << "Hello Road to Redis" << std::endl;
-    return 0;
+    try {
+       
+       std::cout << "hello" <<std::endl;
+    
+      Redis redis("tcp://172.31.94.226");
+      std::cout << "COnnecting\n";
+      redis.set("ahmed", "123");
+      auto val = redis.get("ahmed");    // val is of type OptionalString. See 'API Reference' section for details.
+      if (val) {
+        //Dereference val to get the returned value of std::string type.
+        std::cout << *val << std::endl;
+       }   // else key doesn't exist.
+    }       
+    catch (const Error& err) {
+        // other errors
+        std::cout << err.what() << std::endl;
+
+    }
 }
